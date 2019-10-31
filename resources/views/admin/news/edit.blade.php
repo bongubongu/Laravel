2a7e2b3d840b4b087ca1b862bf176b93ad7685e2
”名前” <>
2019-10-31 06:35:23 +0000

    課題16エラー

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.admin')
-@section('title', 'プロフィールの編集')
+@section('title', 'ニュースの編集')
 
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>プロフィール編集</h2>
-                <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+                <h2>ニュース編集</h2>
+                <form action="{{ action('Admin\NewsController@update') }}" method="post" enctype="multipart/form-data">
                     @if (count($errors) > 0)
                         <ul>
                             @foreach($errors->all() as $e)
@@ -14,36 +14,18 @@
                             @endforeach
                         </ul>
                     @endif
-                    
                     <div class="form-group row">
-                        <label class="col-md-2" for="title">氏名</label>
+                        <label class="col-md-2" for="title">タイトル</label>
                         <div class="col-md-10">
                             <input type="text" class="form-control" name="title" value="{{ $news_form->title }}">
                         </div>
                     </div>
-                    
                     <div class="form-group row">
-                        <label class="col-md-2" for="title">性別</label>
-                        <div class="col-md-10">
-                            <input type="radio" name="gender" value="male">男性
-                            <input type="radio" name="gender" value="female">女性
-                        </div>
-                    </div>
-                    
-                    <div class="form-group row">
-                        <label class="col-md-2" for="title">趣味</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
-                        </div>
-                    </div>
-                    
-                    <div class="form-group row">
-                        <label class="col-md-2" for="body">自己紹介欄</label>
+                        <label class="col-md-2" for="body">本文</label>
                         <div class="col-md-10">
                             <textarea class="form-control" name="body" rows="20">{{ $news_form->body }}</textarea>
                         </div>
                     </div>
-                    
                     <div class="form-group row">
                         <label class="col-md-2" for="image">画像</label>
                         <div class="col-md-10">
@@ -58,7 +40,6 @@
                             </div>
                         </div>
                     </div>
-                    
                     <div class="form-group row">
                         <div class="col-md-10">
                             <input type="hidden" name="id" value="{{ $news_form->id }}">
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('profile/edit', 'Admin\ProfileController@edit');
     Route::post('profile/create', 'Admin\ProfileController@create');
     Route::post('profile/edit', 'Admin\ProfileController@update');
+    Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
 
 
